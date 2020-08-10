bed8931f5aa59d9230ee7cdbbffb370770785081
William Rocha <william0503@gmail.com>
2020-08-09 22:59:43 -0300

    Adiciona flag onlyTracking

--- src/app/controllers/DroneController.js
+++ src/app/controllers/DroneController.js
@@ -1,76 +1,102 @@
 const droneService = require('../services/DroneService');
 
 class DroneController {
-    constructor(){
-        
+  constructor() {}
+
+  async index(req, res) {
+    const { page = 1, onlyTracking = false } = req.query;
+    const drones = await droneService.findAll(page, onlyTracking);
+    return res.json(drones);
+  }
+
+  async show(req, res) {
+    const name = req.params.name;
+    let drone = await droneService.findDroneByName(name);
+    if (!drone) {
+      return res.status(400).send('Drone não cadastrado');
+    }
+    return res.json(drone);
+  }
+
+  async store(req, res) {
+    const { name } = req.body;
+    const { latitude, longitude, temperature, humidity } = req.body;
+
+    let drone = await droneService.findDroneByName(name);
+    if (drone) {
+      return res.status(400).send('Drone com este nome já está cadastrado');
     }
 
-    async index(req, res) {
-        const { page = 1 } = req.query;
-        const drones = await droneService.findAll(page);
-        return res.json(drones);
+    if (temperature < -25 || temperature > 40) {
+      return res
+        .status(400)
+        .send('Temperatura inválida. Deve estar entre -25 e 40');
+    }
+    if (humidity < 0 || humidity > 100) {
+      return res
+        .status(400)
+        .send('Umidade inválida. Deve estar entre 0 e 100.');
+    }
+    if (latitude < -90 || latitude > 90) {
+      return res
+        .status(400)
+        .send('Latitude inválida. Deve estar entre -90 e 90.');
+    }
+    if (longitude < -180 || longitude > 180) {
+      return res
+        .status(400)
+        .send('Longitude inválida. Deve estar entre -180 e 180.');
     }
 
-    async show(req, res) {
-        const name = req.params.name;
-        let drone = await droneService.findDroneByName(name);
-        if (!drone) {
-            return res.status(400).send('Drone não cadastrado');
-        }
-        return res.json(drone);
+    drone = await droneService.createDrone(req.body);
+    return res.status(201).json(drone);
+  }
+
+  async update(req, res) {
+    const name = req.params.name;
+    const { latitude, longitude, temperature, humidity } = req.body;
+
+    let drone = await droneService.findDroneByName(name);
+    if (!drone) {
+      return res.status(400).send('Drone com este nome não está cadastrado');
+    }
+    if (temperature < -25 || temperature > 40) {
+      return res
+        .status(400)
+        .send('Temperatura inválida. Deve estar entre -25 e 40');
+    }
+    if (humidity < 0 || humidity > 100) {
+      return res
+        .status(400)
+        .send('Umidade inválida. Deve estar entre 0 e 100.');
+    }
+    if (latitude < -90 || latitude > 90) {
+      return res
+        .status(400)
+        .send('Latitude inválida. Deve estar entre -90 e 90.');
+    }
+    if (longitude < -180 || longitude > 180) {
+      return res
+        .status(400)
+        .send('Longitude inválida. Deve estar entre -180 e 180.');
     }
 
-    async store(req, res) {
+    drone = await droneService.findByIdAndUpdate(drone._id, req.body, {
+      new: true,
+    });
+    return res.json(drone);
+  }
 
-        const { name } = req.body
-        const { temperature } = req.body
-        const { humidity } = req.body
-        const { latitude } = req.body
-        const { longitude } = req.body
-
-        let drone = await droneService.findDroneByName(name);
-        if (drone) { return res.status(400).send('Drone com este nome já está cadastrado'); }
-
-        if ((temperature < -25) || (temperature > 40)) { return res.status(400).send('Temperatura inválida. Deve estar entre -25 e 40'); }
-        if ((humidity < 0) || (humidity > 100)) { return res.status(400).send('Umidade inválida. Deve estar entre 0 e 100.'); }
-        if ((latitude < -90) || (latitude > 90)) { return res.status(400).send('Latitude inválida. Deve estar entre -90 e 90.'); }
-        if ((longitude < -180) || (longitude > 180)) { return res.status(400).send('Longitude inválida. Deve estar entre -180 e 180.'); }
-
-        drone = await droneService.createDrone(req.body)
-        return res.status(201).json(drone);
-
+  async destroy(req, res) {
+    const name = req.params.name;
+    let drone = await droneService.findDroneByName(name);
+    if (!name) {
+      return res.status(400).send('Drone com este nome não está cadastrado');
     }
 
-    async update(req, res) {
-
-        const name = req.params.name;
-        const { temperature } = req.body
-        const { humidity } = req.body
-        const { latitude } = req.body
-        const { longitude } = req.body
-
-        let drone = await droneService.findDroneByName(name);
-        if (!drone) { return res.status(400).send('Drone com este nome não está cadastrado'); }
-        if ((temperature < -25) || (temperature > 40)) { return res.status(400).send('Temperatura inválida. Deve estar entre -25 e 40'); }
-        if ((humidity < 0) || (humidity > 100)) { return res.status(400).send('Umidade inválida. Deve estar entre 0 e 100.'); }
-        if ((latitude < -90) || (latitude > 90)) { return res.status(400).send('Latitude inválida. Deve estar entre -90 e 90.'); }
-        if ((longitude < -180) || (longitude > 180)) { return res.status(400).send('Longitude inválida. Deve estar entre -180 e 180.'); }
-
-        drone = await droneService.findByIdAndUpdate(drone._id, req.body, { new: true });
-        return res.json(drone);
-    }
-
-    async destroy(req, res) {
-        const name = req.params.name;
-        let drone = await droneService.findDroneByName(name)
-        if (!name) {
-            return res.status(400).send('Drone com este nome não está cadastrado');
-        }
-
-        drone = await droneService.findByIdAndRemove(drone._id);
-        return res.send();
-    }
-
+    drone = await droneService.findByIdAndRemove(drone._id);
+    return res.send();
+  }
 }
 
-module.exports = new DroneController()
+module.exports = new DroneController();
--- src/app/services/DroneService.js
+++ src/app/services/DroneService.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose')
-var Drone = require('../models/Drone')
+const mongoose = require('mongoose');
+var Drone = require('../models/Drone');
 
 class DroneService {
-    constructor(){
-        
-    }
-    async findOne(query) {
-        return await Drone.findOne(query)
-    }
+  constructor() {}
+  async findOne(query) {
+    return await Drone.findOne(query);
+  }
 
-    async find(query, page) {
-        return await Drone.paginate(query, { page, limit:10 })
-    }
+  async find(query, page) {
+    return await Drone.paginate(query, { page, limit: 10 });
+  }
 
-    async findAll(page) {
-        return await Drone.find()
-    }
-    
-    async createDrone(drone){
-        return await Drone.create(drone)
-    }
-    
-    async findByIdAndRemove(id){
-        return await Drone.findByIdAndRemove(id);
-    }
+  async findAll(page, onlyTracking = false) {
+    const drones = await Drone.find();
+    if (onlyTracking) return drones.filter((drone) => drone.tracking == true);
 
-    async findByIdAndUpdate(id, drone){
-        return await Drone.findByIdAndUpdate(id, drone, { new: true })
-    }
+    return drones;
+  }
 
-    async findDroneByName(name) {
-        return await Drone.findOne({name: name})
-    }
+  async createDrone(drone) {
+    return await Drone.create(drone);
+  }
 
-    async findByTracking(condiction) {
-        return await Drone.find({tracking : condiction})
-    }
+  async findByIdAndRemove(id) {
+    return await Drone.findByIdAndRemove(id);
+  }
 
+  async findByIdAndUpdate(id, drone) {
+    return await Drone.findByIdAndUpdate(id, drone, { new: true });
+  }
+
+  async findDroneByName(name) {
+    return await Drone.findOne({ name: name });
+  }
+
+  async findByTracking(condiction) {
+    return await Drone.find({ tracking: condiction });
+  }
 }
 
-module.exports = new DroneService()
+module.exports = new DroneService();
